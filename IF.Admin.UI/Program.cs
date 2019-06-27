d9d3d7a46910b6048b61000660ee50e775e12b6b
Caglar <caglarcagli@derinbilgi.com.tr>
2019-06-27 17:15:38 +0300

    sdvsd

--- IF.Admin.UI/Program.cs
+++ IF.Admin.UI/Program.cs
@@ -20,7 +20,7 @@ namespace TutumluAnne.Log.AdminUI
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5003");
+                .UseUrls("http://localhost:5002");
     }
 
 
