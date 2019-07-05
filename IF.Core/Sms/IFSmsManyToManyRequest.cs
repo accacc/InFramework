5cc60aa170e14c3c56c65618c726c293f05d3607
Caglar <caglarcagli@derinbilgi.com.tr>
2019-07-05 16:40:16 +0300

    manytomany

--- IF.Core/Sms/IFSmsManyToManyRequest.cs
+++ IF.Core/Sms/IFSmsManyToManyRequest.cs
@@ -5,12 +5,27 @@ using System.Text;
 
 namespace IF.Core.Sms
 {
-    public class IFSmsManyToManyRequest: BaseRequest
+    public class IFSmsManyToManyRequest : BaseRequest
     {
         public string Subject { get; set; }
 
-        public string Message { get; set; }
+
 
         public Guid SourceId { get; set; }
+
+
+
+
+        public List<IFSmsManyToManyModel> Messages { get; set; }
     }
+
+    public class IFSmsManyToManyModel
+    {
+        public string Message { get; set; }
+
+        public string Number { get; set; }
+    }
+
+
+
 }
