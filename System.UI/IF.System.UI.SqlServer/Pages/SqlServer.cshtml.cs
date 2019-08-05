c16eae90174e07f217cb8b96f1ae7dccf315b7e1
Caglar <caglarcagli@derinbilgi.com.tr>
2019-08-05 15:11:25 +0300

    assa

--- System.UI/IF.System.UI.SqlServer/Pages/SqlServer.cshtml.cs
+++ System.UI/IF.System.UI.SqlServer/Pages/SqlServer.cshtml.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace TutumluAnne.Log.AdminUI.Pages
+namespace IF.System.UI.SqlServer.Pages
 {
 
     //[Authorize]
