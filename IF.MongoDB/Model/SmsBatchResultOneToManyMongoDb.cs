34c4a60b1999d461be67498a3ed6dd946530fc83
Caglar <caglarcagli@derinbilgi.com.tr>
2019-08-27 11:33:21 +0300

    asdas

---
+++ IF.MongoDB/Model/SmsBatchResultOneToManyMongoDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IF.Core.Sms;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IF.MongoDB.Model
+{
+    public class SmsBatchResultOneToManyMongoDb : ISmsBatchResultOneToMany
+    {
+
+        [BsonId]
+        public ObjectId InternalId { get; set; }
+
+        public int BatchNumber { get; set; }
+        public string BatchName { get; set; }
+        public int BatchCount { get; set; }
+
+        public SmsOperationStatus Status { get; set; }
+
+        public string ErrorCode { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public Guid SourceId { get; set; }
+
+        public string IntegrationId { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+    }
+}
--- IF.MongoDB/Model/SmsLog.cs
+++ IF.MongoDB/Model/SmsLog.cs
@@ -33,64 +33,7 @@ namespace IF.MongoDB.Model
 
    
 
-    public class SmsBulkOneToManyOperationMongoDb: ISmsBulkOneToManyOperation
-    {
-        [BsonId]
-        public ObjectId InternalId { get; set; }
+    
 
-        //public IFormFile File { get; set; }
-        public string BulkName { get; set; }
-
-        public int SplitBy { get; set; }
-
-        public long Total { get; set; }
-        public string Message { get; set; }
-
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
-
-        public SmsOperationStatus Status { get; set; }
-
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
-        public DateTime UpdatedDate { get; set; } = DateTime.Now;
-
-        public int BatchCount { get; set; }
-
-        public string SenderPrefixName { get; set; }
-
-        public string CallBackPrefixName { get; set; }
-
-        public string CallBackMessageTemplate { get; set; }
-
-        public string CallBackNumberId { get; set; }
-
-        public DateTime? StartDate { get; set; }
-
-        public DateTime? EndDate { get; set; }
-    }
-
-    public class SmsBatchResultOneToManyMongoDb : ISmsBatchResultOneToMany
-    {
-
-        [BsonId]
-        public ObjectId InternalId { get; set; }
-
-        public int BatchNumber { get; set; }
-        public string BatchName { get; set; }
-        public int BatchCount { get; set; }
-
-        public SmsOperationStatus Status { get; set; }
-
-        public string ErrorCode { get; set; }
-
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
-
-        public Guid SourceId { get; set; }
-
-        public string IntegrationId { get; set; }
-
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
-        public DateTime UpdateDate { get; set; } = DateTime.Now;
-    }
+   
 }
