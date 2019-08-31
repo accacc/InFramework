7d52a3d9192abf7dd24c110e5e20f3ae857903f8
Ali Çağlar Çağlı <bluetonice@hotmail.com>
2019-08-31 12:29:17 +0300

    asdsa

--- IF.Core/Sms/IFBatchItemModel.cs
+++ IF.Core/Sms/IFBatchItemModel.cs
@@ -4,18 +4,19 @@ using System.Text;
 
 namespace IF.Core.Sms
 {
-    public class EmailModel
+    public class IFBatchItemModel
     {
 
 
         //public string Message { get; set; }
-        public string Email { get; set; }
+        //public string Email { get; set; }
         public SmsState State { get; set; }
 
         //public string CallbackMessage { get; set; }
 
         public Dictionary<string,string> Parameters { get; set; }
-        public DateTime SentDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
 
 
 
--- IF.Core/Sms/IFEmailOnetoManyRequest.cs
+++ IF.Core/Sms/IFEmailOnetoManyRequest.cs
@@ -5,22 +5,5 @@ using System.Text;
 
 namespace IF.Core.Sms
 {
-    public class IFEmailOneToManyRequest : BaseRequest
-    {
-        //public string Subject { get; set; }
-
-        //public string Message { get; set; }
-
-        public Guid SourceId { get; set; }
-
-        //public DateTime? StartDate { get; set; }
-
-        //public DateTime? EndDate { get; set; }
-        public List<EmailModel> Emails { get; set; }
-
-        public int EventId { get; set; }
-
-        
-
-    }
+   
 }
