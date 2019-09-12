c7c84fba92666696f1fe8d9ec0ae4324f6903e1f
Ali Çağlar Çağlı <bluetonice@hotmail.com>
2019-09-12 21:53:51 +0300

    asdas

--- IF.Core/Sms/IFBulkOperation.cs
+++ IF.Core/Sms/IFBulkOperation.cs
@@ -15,7 +15,7 @@ namespace IF.Core.Sms
         public int SplitBy { get; set; }
 
         public long Total { get; set; }
-        public string Message { get; set; }
+        //public string Message { get; set; }
 
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
@@ -24,17 +24,17 @@ namespace IF.Core.Sms
         public int BatchCount { get; set; }
 
         public string Type { get; set; }
-        public string SenderPrefixName { get; set; }
+        //public string SenderPrefixName { get; set; }
 
-        public string CallBackPrefixName { get; set; }
+        //public string CallBackPrefixName { get; set; }
 
-        public string CallBackMessageTemplate { get; set; }
+        //public string CallBackMessageTemplate { get; set; }
 
-        public string CallBackNumberId { get; set; }
+        //public string CallBackNumberId { get; set; }
 
-        public DateTime? StartDate { get; set; }
+        //public DateTime? StartDate { get; set; }
 
-        public DateTime? EndDate { get; set; }
+        //public DateTime? EndDate { get; set; }
 
 
     }
