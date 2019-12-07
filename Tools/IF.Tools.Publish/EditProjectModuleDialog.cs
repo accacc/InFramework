b3fa04f6cd9bd450156cdf4116592d528f3542c3
caglar <caglar@caglar.com>
2019-12-07 13:21:18 -0800

    add template editor project .net core version

--- Tools/IF.Tools.Publish/EditProjectModuleDialog.cs
+++ Tools/IF.Tools.Publish/EditProjectModuleDialog.cs
@@ -1,14 +1,7 @@
 ﻿using IF.Core.Json;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IF.Tools.Publish
---
+++ Tools/IF.Tools.Templates.Editor/Form1.cs
@@ -0,0 +1,58 @@
+﻿using IF.Core.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace IF.Tools.Templates.Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BindComboBox();
+           
+        }
+
+
+        private void BindComboBox()
+        {
+            List<NameValueDto> projectTemplates = new List<NameValueDto>();
+           
+
+            using (var ctx = new MyDbContext())
+            {
+               
+
+                foreach (var template in ctx.ProjectTemplates.ToList())
+                {
+                    projectTemplates.Add(new NameValueDto {Name = template.Name,Value = template.Code });
+
+                }
+            }
+
+
+
+            templateTypeBindingSource.DataSource = projectTemplates;
+
+
+            comboBoxTemplates.DisplayMember = "Name";
+            comboBoxTemplates.ValueMember = "Value";
+            comboBoxTemplates.DataSource = templateTypeBindingSource;
+
+
+           
+        }
+
+        private void buttonSelectTemplate_Click(object sender, System.EventArgs e)
+        {
+            if(comboBoxTemplates.SelectedValue.ToString() == "WA")
+            {
+                WebApiTemplateDialog webApiTemplateDialog = new WebApiTemplateDialog();
+                webApiTemplateDialog.ShowDialog();
+
+            }
+        }
+    }
+}
