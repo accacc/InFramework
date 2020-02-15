b644f3a2bf70251c7fe69df7867e3cfda5d29949
caglar <accacc@gmail.com>
2020-02-16 02:07:27 +0300

    fixes

--- Tools/IF.Tools.CodeGenerator/MvcGetGeneratorForm.cs
+++ Tools/IF.Tools.CodeGenerator/MvcGetGeneratorForm.cs
@@ -1,74 +1,17 @@
-﻿using IF.CodeGeneration.Application.Generator;
-using IF.CodeGeneration.Application.Generator.Get;
-using IF.CodeGeneration.Application.Generator.List;
-using IF.CodeGeneration.Application.Generator.Update;
-using System;
-using System.Linq;
-using System.Web.UI.WebControls;
+﻿using IF.CodeGeneration.Application.Generator.Get;
+
 using System.Windows.Forms;
 
 namespace IF.Tools.CodeGenerator
 {
-    public partial class MvcGetGeneratorForm : Form
+    public partial class MvcGetGeneratorForm : ApiGeneratorBaseForm
     {
 
-        public CSGetGenerator generator { get; set; }
-
-        public MvcGetGeneratorForm(CSGetGenerator generator)
-        {
-            InitializeComponent();
-
-            generator.UpdateContext();
-
-            textBoxViewBasePath.Text = @"Views/Security/User";
-            textBoxControllerName.Text = "SecurityController";          
-
-            this.generator = generator;
-
-            this.checkedListBoxVsFiles.Items.Clear();
-
-            foreach (var item in generator.Context.Files)
-            {
-                this.checkedListBoxVsFiles.Items.Add(item.FileType);
-            }
-
-            for (int i = 0; i < checkedListBoxVsFiles.Items.Count; i++)
-            {
-                checkedListBoxVsFiles.SetItemChecked(i, true);
-            }
-        }
-
-        private void buttonGenerate_Click(object sender, EventArgs e)
+        public MvcGetGeneratorForm(CSGetGenerator generator) : base(generator)
         {
 
-            if (String.IsNullOrWhiteSpace(textBoxViewBasePath.Text))
-            {
-                MessageBox.Show(@"Please enter the View Base Path.", @"Required", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                return;
-            }
-
-
-            if (String.IsNullOrWhiteSpace(textBoxControllerName.Text))
-            {
-                MessageBox.Show(@"Please enter the ControllerName.", @"Required", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                return;
-            }
-
-            generator.Context.RepositoryName = textBoxViewBasePath.Text;
-            generator.Context.ControllerName = textBoxControllerName.Text;
-            
-            generator.UpdateContext();
-
-            foreach (var item in checkedListBoxVsFiles.CheckedItems)
-            {
-                var vsFile = generator.Context.Files.SingleOrDefault(f => f.FileType == (VSFileType)item);
-                if (vsFile != null)
-                {
-                    this.generator.SetItemActive(vsFile.FileType);
-                }
-            }
-              
-            generator.Generate();
         }
+        
+       
     }
 }
--- Tools/IF.Tools.CodeGenerator/MvcUpdateGeneratorForm.cs
+++ Tools/IF.Tools.CodeGenerator/MvcUpdateGeneratorForm.cs
@@ -8,66 +8,13 @@ using System.Windows.Forms;
 
 namespace IF.Tools.CodeGenerator
 {
-    public partial class MvcUpdateGeneratorForm : Form
+    public partial class MvcUpdateGeneratorForm : ApiGeneratorBaseForm
     {
-
-        public CSUpdateGenerator generator { get; set; }
-
-        public MvcUpdateGeneratorForm(CSUpdateGenerator generator)
-        {
-            InitializeComponent();
-
-            generator.UpdateContext();
-
-            textBoxViewBasePath.Text = @"Views/Security/User";
-            textBoxControllerName.Text = "SecurityController";          
-
-            this.generator = generator;
-
-            this.checkedListBoxVsFiles.Items.Clear();
-
-            foreach (var item in generator.Context.Files)
-            {
-                this.checkedListBoxVsFiles.Items.Add(item.FileType);
-            }
-
-            for (int i = 0; i < checkedListBoxVsFiles.Items.Count; i++)
-            {
-                checkedListBoxVsFiles.SetItemChecked(i, true);
-            }
-        }
-
-        private void buttonGenerate_Click(object sender, EventArgs e)
+        public MvcUpdateGeneratorForm(CSUpdateGenerator generator) : base(generator)
         {
 
-            if (String.IsNullOrWhiteSpace(textBoxViewBasePath.Text))
-            {
-                MessageBox.Show(@"Please enter the View Base Path.", @"Required", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                return;
-            }
-
-
-            if (String.IsNullOrWhiteSpace(textBoxControllerName.Text))
-            {
-                MessageBox.Show(@"Please enter the ControllerName.", @"Required", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                return;
-            }
-
-            generator.Context.RepositoryName = textBoxViewBasePath.Text;
-            generator.Context.ControllerName = textBoxControllerName.Text;
-            
-            generator.UpdateContext();
-
-            foreach (var item in checkedListBoxVsFiles.CheckedItems)
-            {
-                var vsFile = generator.Context.Files.SingleOrDefault(f => f.FileType == (VSFileType)item);
-                if (vsFile != null)
-                {
-                    this.generator.SetItemActive(vsFile.FileType);
-                }
-            }
-              
-            generator.Generate();
         }
+
+  
     }
 }
