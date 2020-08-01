2baa85702a4738a88fd1cb07a9460b431b802ce0
caglar <accacc@gmail.com>
2020-08-02 02:59:53 +0300

    fixes

---
+++ IF.Core/Control/PageControlType.cs
@@ -0,0 +1,18 @@
+﻿namespace IF.Core.Control
+{
+    public enum PageControlType
+    {
+        Page = 6,
+        Grid = 0,
+        Form = 1,
+        Action = 2,
+        MenuItem = 3,
+        DropDown = 4,
+        MultiSelect = 5,
+        ListView = 7,
+        Accordion = 8,
+        TreeView = 9
+
+
+    }
+}
---
+++ IF.Core/Navigation/NavigationDto.cs
@@ -0,0 +1,21 @@
+﻿using IF.Core.Control;
+using IF.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IF.Core.Navigation
+{
+    public class NavigationDto: TreeDto<NavigationDto>
+    {
+
+        public string Name { get; set; }
+        public int PageControlId { get; set; }
+
+        public PageControlType ControlType { get; set; }       
+
+        public string Description { get; set; }
+
+        public string ClientId { get; set; }
+    }
+}
